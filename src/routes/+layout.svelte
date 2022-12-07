ca429da9df7f4425c8fd9795b77a7a37a9006f49
EdwardGold Edem Hogan <edge37@outlook.com>
2022-12-07 15:57:28 +0100

    ..

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import 'carbon-components-svelte/css/all.css';
+
+	import 'carbon-components-svelte/css/g100.css';
+	import 'carbon-components-svelte/css/white.css';
+
+	import { Theme } from '$lib/components';
+</script>
+
+<Theme>
+	<slot />
+</Theme>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,83 @@
-<script lang='ts'>
-    import wiki from 'wikipedia'
+<script lang="ts">
+	import wiki from 'wikipedia';
 
-    import { ComboBox } from 'carbon-components-svelte'
+	import { Button, ComboBox } from 'carbon-components-svelte';
+	import Pause from 'carbon-icons-svelte/lib/Pause.svelte';
+	import Play from 'carbon-icons-svelte/lib/Play.svelte';
 
-    let value: string, results = any[], search_on_input_interval_id: number
+	/*
+        words loading
+        pause
+        {prev
+        next}
+            on:keydown set interval do
+            on:keyup clear interval
+    */
 
-    const search_on_input = () => {
-        clearInterval(search_on_input_interval_id)
-        search_on_input_interval_id = setInterval(()=>{
-            search()
-        }, 1777)
-    }
+	interface Result {
+		id: number;
+		text: string;
+		pageid: number;
+	}
 
-    const search = async() => {
-        ({results} = await wiki.search(value, {limit: 7}).catch(() => []))
-        console.log(results)
-    }
+	let value: string,
+		results: Result[],
+		search_delay_id: NodeJS.Timer,
+		content: string,
+		words: string[],
+		display: string,
+		paused: boolean = true,
+		word_interval: number = 200.0415430267062,
+		word_interval_id: NodeJS.Timer,
+		current_index: number = 0;
+
+	const search_on_input = () => {
+		typeof search_delay_id === 'number' ? clearInterval(search_delay_id) : {};
+		search_delay_id = setTimeout(() => {
+			search();
+		}, 1000);
+	};
+
+	const select = async ({ detail }) => {
+		content = await wiki.content(detail.selectedItem.text);
+		pause();
+		words = content.split(/\s+/g);
+		current_index = 0;
+	};
+
+	const toggle = () => {
+		paused ? play() : pause();
+	};
+
+	const pause = () => clearInterval(word_interval_id);
+
+	const play = () => {
+		word_interval_id = setInterval(() => {
+			let next = words[current_index];
+			if (next) display = next;
+			++current_index;
+		}, word_interval);
+	};
+
+	const search = async () => {
+		let { results: res } = await wiki.search(value, { limit: 7 }).catch(() => []);
+		console.log('res', res);
+		if (res)
+			results = res.map((r, i) => {
+				return {
+					id: i,
+					text: r.title,
+					pageid: r.pageid
+				};
+			});
+		console.log(results);
+	};
+
+	search();
 </script>
 
-<ComboBox bind:value items={results}
+<ComboBox on:keydown={search_on_input} on:select={select} bind:value items={results} />
+
+<p>{display}</p>
+
+<Button icon={paused ? Play : Pause} on:click={toggle} />
