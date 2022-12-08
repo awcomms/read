97264cc47aafd5c5c33734e48666bbc0f4545ef8
EdwardGold Edem Hogan <edge37@outlook.com>
2022-12-08 10:09:10 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,29 @@
 <script lang="ts">
 	import wiki from 'wikipedia';
 
-	import { Button, ComboBox, InlineLoading, NumberInput } from 'carbon-components-svelte';
+	import {
+		Button,
+		ComboBox,
+		InlineLoading,
+		NumberInput,
+		Slider,
+		Toggle
+	} from 'carbon-components-svelte';
 	import Pause from 'carbon-icons-svelte/lib/Pause.svelte';
 	import Play from 'carbon-icons-svelte/lib/Play.svelte';
 	import Reset from 'carbon-icons-svelte/lib/Reset.svelte';
 	import ChevronLeft from 'carbon-icons-svelte/lib/ChevronLeft.svelte';
 	import ChevronRight from 'carbon-icons-svelte/lib/ChevronRight.svelte';
 
+	import type { ComboBoxItem } from 'carbon-components-svelte/types/ComboBox/ComboBox.svelte';
+
 	interface Item {
 		id: number;
 		text: string;
 		pageid?: number;
 	}
+
+	const calc_speed = (v: number) => 60000 / v;
 
 	$: shift(current_index);
 	$: update_speed(word_interval);
@@ -24,13 +35,21 @@
 		content: string,
 		words: string[],
 		search_loading: boolean,
+		show_content: boolean,
 		loading: boolean,
-		wpm: number,
 		display: string,
 		paused: boolean = true,
 		word_interval: number = 200.0415430267062,
+		wpm: number = calc_speed(word_interval),
 		word_interval_id: NodeJS.Timer,
 		current_index: number = 0;
+
+	// const keydown = (e: KeyboardEvent) => {
+	// 	if (e.key === 'Enter') {
+	//         console.log('i')
+	// 		toggle();
+	// 	}
+	// };
 
 	const update_speed = (..._: number[]) => {
 		pause();
@@ -44,7 +63,8 @@
 		}, 1000);
 	};
 
-	const select = async ({ detail }) => {
+	const select = async (e: CustomEvent<{ selectedId: any; selectedItem: ComboBoxItem }>) => {
+		const { detail } = e;
 		loading = true;
 		content = await wiki.content(detail.selectedItem.text);
 		pause();
@@ -111,21 +131,27 @@
 
 	const search = async () => {
 		search_loading = true;
-		let { results: res } = await wiki.search(value, { limit: 7 }).catch(() => []);
-		console.log('res', res);
-		if (res)
-			items = res.map((r, i) => {
-				return {
-					id: i,
-					text: r.title,
-					pageid: r.pageid
-				};
-			});
-		search_loading = true;
+		await wiki
+			.search(value, { limit: 7 })
+			.catch(() => [])
+			.then((r) => {
+				let res = r.results;
+				if (res)
+					items = res.map((r, i) => {
+						return {
+							id: i,
+							text: r.title,
+							pageid: r.pageid
+						};
+					});
+			})
+			.finally(() => (search_loading = false));
 	};
 
 	search();
 </script>
+
+<!-- <svelte:window on:keydown={keydown} /> -->
 
 <ComboBox on:keydown={search_on_input} on:select={select} bind:value {items} />
 
@@ -137,6 +163,7 @@
 	<p>{display}</p>
 
 	{#if words && words.length > 0}
+		<Slider light bind:value={current_index} max={words.length - 1} />
 		<Button size="small" iconDescription="Previous" on:click={prev} icon={ChevronLeft} />
 		<Button size="small" iconDescription="Next" on:click={next} icon={ChevronRight} />
 	{/if}
@@ -151,18 +178,27 @@
 {/if}
 
 <NumberInput
-	on:input={({ detail }) => (word_interval = 60000 / wpm)}
+	on:input={({ detail }) => (word_interval = calc_speed(wpm))}
 	bind:value={wpm}
 	size="sm"
 	label="Words per minute"
 	min={1}
 />
 <NumberInput
-	on:input={({ detail }) => (wpm = 60000 / word_interval)}
+	on:input={({ detail }) => (wpm = calc_speed(word_interval))}
 	bind:value={word_interval}
 	size="sm"
 	label="Word interval"
 	min={1}
 	helperText="Words change every {word_interval} millisecond{word_interval > 1 ? 's' : ''}"
 />
-<p>{60 / (word_interval / 1000)} words per minute</p>
+
+<br />
+
+{#if content}
+	<Toggle size="sm" bind:toggled={show_content} labelText="Show content" />
+	<br />
+	{#if show_content}
+		<p>{content}</p>
+	{/if}
+{/if}
