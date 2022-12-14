de552c0898a6f851a343f2d254161da3c41057ae
EdwardGold Edem Hogan <edge37@outlook.com>
2022-12-14 15:47:42 +0100

    haa

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Theme } from './Theme.svelte';
 export { default as Select } from './Select.svelte';
+export { default as Words } from './Words.svelte';
---
+++ src/routes/raw/+page.svelte
@@ -0,0 +1,10 @@
+<script lang='ts'>
+    import {Words} from "$lib/components"
+    import { TextArea } from "carbon-components-svelte";
+
+    let content: string
+</script>
+
+<TextArea bind:value={content} />
+<Words {content} />
+
